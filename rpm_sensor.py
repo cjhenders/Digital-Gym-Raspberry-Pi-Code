ad4e4398343455c57fb2d83a675cde8065d74d7f
Carl Henderson <cjhenders@gmail.com>
2017-04-13 23:18:55 -0500

    #Revised the Code to add try catch to stop network errors

--- rpm_sensor.py
+++ rpm_sensor.py
@@ -27,16 +27,17 @@ def sensorCallback1(channel):
     print current_time
     rpm = (1 / (current_time - last_time)) * 60
     print rpm
-    if ((1 / (current_time - last_time))*60 < 200) and ((1 / (current_time - last_time))*60 > 10):
-        rpm = (1 / (current_time - last_time)) * 60
-        print rpm
-        post_data = {"rpm": rpm, "bikeId": "1"}
-        try:
-            r = requests.post(url=API_ENDPOINT, data=post_data)
-        except requests.exceptions.RequestException as e:
-            print e
+    if ((1 / (current_time - last_time))*60 < 200):
+        if ((1 / (current_time - last_time))*60 > 10):
+            rpm = (1 / (current_time - last_time)) * 60
+            print rpm
+            post_data = {"rpm": rpm, "bikeId": "1"}
+            try:
+                r = requests.post(url=API_ENDPOINT, data=post_data)
+            except requests.exceptions.RequestException as e:
+                print e
 
-        last_time = current_time
+            last_time = current_time
 
 
 def main():
