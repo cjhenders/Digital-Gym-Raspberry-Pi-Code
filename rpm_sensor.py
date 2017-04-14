3576239e1ed045c31d49649fe1df37e5d4efb500
Carl Henderson <cjhenders@gmail.com>
2017-04-13 22:54:39 -0500

    #Revised the Code to add try catch to stop network errors

--- rpm_sensor.py
+++ rpm_sensor.py
@@ -24,7 +24,7 @@ def sensorCallback1(channel):
 
 
     current_time = time.time()
-    rpm = (1 / (current_time - last_time)) * 60
+    print rpm = (1 / (current_time - last_time)) * 60
     if ((1 / (current_time - last_time)) * 60 < 200) and ((1 / (current_time - last_time)) * 60 > 10):
         rpm = (1 / (current_time - last_time)) * 60
         print rpm
