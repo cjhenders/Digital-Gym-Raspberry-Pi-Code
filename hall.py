4d7a91b096d983a031846bb9902233f899664569
Carl Henderson <cjhenders@gmail.com>
2017-04-13 22:19:40 -0500

    #Revised the Code to add try catch to stop network errors

--- hall.py
+++ hall.py
@@ -24,6 +24,8 @@ import datetime
 import requests
 import urllib
 import httplib
+import smtplib
+from email.mime.text import MIMEText
 
 global last_time
 
@@ -33,73 +35,84 @@ API_ENDPOINT = "http://52.34.141.31:8000/bbb/bike"
 
 def sensorCallback1(channel):
   
-  global last_time
-  global miss
+	global last_time
+	global miss
 
-  miss =0
-  if not last_time:
-    last_time = 0
+	miss =0
+	if not last_time:
+	last_time = 0
  
-  # Called if sensor output goes LOW
-  timestamp = time.time()
-  stamp = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
-  current_time = time.time()
-  if((1/(current_time - last_time))*60 < 200):
-    if((1/(current_time - last_time))*60 > 10):
-      rpm = (1/(current_time - last_time))*60
-      print rpm
-      data = {"rpm": rpm,
-              "bikeId": "1"}
-      # Send Post request and save the response as a response object 
-      r = requests.post(url = API_ENDPOINT, data = data)
-
-      #extracting response text
-      pastebin_url = r.text
-      print("the pastebin URL is: %s"%pastebin_url)
-    last_time = current_time
+	# Called if sensor output goes LOW
+	current_time = time.time()
+	if ((1 / (current_time - last_time)) * 60 < 200) and ((1 / (current_time - last_time)) * 60 > 10):
+        # Send Post request and save the response as a response object
+        rpm = (1/(current_time - last_time))*60
+        print rpm
+        data = {"rpm": rpm, "bikeId": "1"}
+        try:
+            r = requests.post(url = API_ENDPOINT, data = data)
+        except requests.exceptions.RequestException as e:
+            print "Connection Error"
+            if email == False:
+                email = True
+                msg["Subject"] = "Pi Bike #1 has had a connection error"
+                msg["From"] = cjhenders @ gmail.com
+                msg["To"] = cjhenders @ gmail.com
+                s = smtplib.SMTP("localhost")
+                s.sendmail(cjhenders @ gmail.com, [cjhenders @ gmail.com], msg.as_string())
+                s.quit
+			#extracting response text
+			pastebin_url = r.text
+			print("the pastebin URL is: %s"%pastebin_url)
+			last_time = current_time
   
 
-'''
-def sensorCallback2(channel):
-  # Called if sensor output goes HIGH
-  timestamp = time.time()
-  stamp = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S')
-  print "Sensor HIGH " + stamp
-'''
 
 
-def main():
-  global miss
-  miss = 0
-  # Wrap main content in a try block so we can
-  # catch the user pressing CTRL-C and run the
-  # GPIO cleanup function. This will also prevent
-  # the user seeing lots of unnecessary error
-  # messages.
+def main():  
+	global miss
+	global email
+	email = False
+	miss = 0
+	# Wrap main content in a try block so we can
+	# catch the user pressing CTRL-C and run the
+	# GPIO cleanup function. This will also prevent
+	# the user seeing lots of unnecessary error
+	# messages.
   
   
-  
-  
-  try:
-    # Loop until users quits with CTRL-C
-    while True :
-      miss+=1
-      time.sleep(0.1)
-      if miss == 30:
+	try:
+		# Loop until users quits with CTRL-C
+		while True :
+			miss+=1
+			time.sleep(0.1)
+			if miss == 30:
 
-        data = {"rpm": 0,
-              "bikeId": "1"}
-        # Send Post request and save the response as a response object 
-        r = requests.post(url = API_ENDPOINT, data = data)
+				data = {"rpm": 0,
+					"bikeId": "1"}
+			try:        
+				r = requests.post(url = API_ENDPOINT, data = data)
+				email = False
+			except requests.exceptions.RequestException as e:
+				print "Connection Error"
+				if email == False:
+					email = True
+					msg["Subject"] = "Pi Bike #1 has had a connection error"
+					msg["From"] = cjhenders@gmail.com
+					msg["To"] = cjhenders@gmail.com
+					s = smtplib.SMTP("localhost")
+					s.sendmail(cjhenders@gmail.com,[cjhenders@gmail.com],msg.as_string())
+					s.quit
+			# Send Post request and save the response as a response object
+			#extracting response text
 
-        #extracting response text
-        pastebin_url = r.text
-        print("0 Response %s"%pastebin_url)
-        
+			pastebin_url = r.text
+		    print("0 Response %s"%pastebin_url)
+
 
   except KeyboardInterrupt:
     # Reset GPIO settings
-    GPIO.cleanup()  
+	GPIO.cleanup()
   
 # Tell GPIO library to use GPIO references
 GPIO.setmode(GPIO.BCM)
--- nfc_code.py
+++ nfc_code.py
@@ -4,7 +4,6 @@ Code to read RFID Tags for Rasp Pi
 Carl Henderson Feb 2017
 """
 
-
 import nfc
 import time
 import datetime
@@ -17,18 +16,22 @@ def connected(tag):
     print(tag);
     return False
 
+
 clf = nfc.ContactlessFrontend('usb')
 
 while True:
-
     API_ENDPOINT = API_ENDPOINT = "http://52.34.141.31:8000/bbb/addsession"
     API_KEY = "ashu1234"
-    
+
     tag = clf.connect(rdwr={'on-connect': connected})
     data = {"tag": tag, "bikeId": 1}
-    
-    r = requests.post(url = API_ENDPOINT, data = data)
+    try:
+        r = requests.post(url=API_ENDPOINT, data=data)
+    except requests.exceptions.RequestException as e:
+        print e
 
-    #extracting response text
+
+
+    # extracting response text
     pastebin_url = r.text
-    print("the pastebin URL is: %s"%pastebin_url)
+    print("the pastebin URL is: %s" % pastebin_url)
